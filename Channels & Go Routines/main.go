7ae6132ea9ed9cdca8081844e468d68a55407a32
Adham-Niazy <adhamniazy@gmail.com>
2022-09-11 03:32:56 +0200

    Go Routines and channels practice

---
+++ Channels & Go Routines/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		// Literal Functions in go === Anonymous Functions in JavaScript
+		go func (link string)  {
+			time.Sleep(2 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "Might be down!")
+		c <- (link)
+		return
+	}
+	c <- (link)
+	fmt.Println(link, "is UP!")
+}
